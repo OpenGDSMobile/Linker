f52e0db0b87b273aaf3bb74197670e34de1abc8e
motobyus <motobyus@naver.com>
2014-11-07 12:06:28 +0000

    save

--- ubuntu12.04/c_userConfigure.sh
+++ ubuntu12.04/c_userConfigure.sh
@@ -29,10 +29,10 @@ done < $settingFilename
 tomcat7_userControlFile="/var/lib/tomcat7/conf/tomcat-users.xml"
 userInfo="<user username=\"$username\" password=\"$password\" roles=\"admin,manager,manager-gui\"/>"
 
-sed -i '19i\  <role rolename="manager"/>' $tomcat7_userControlFile
-sed -i '20i\  <role rolename="admin"/>' $tomcat7_userControlFile
-sed -i '21i\  <role rolename="manager-gui"/>' $tomcat7_userControlFile
-sed -i "22i\  $userInfo" $tomcat7_userControlFile
+#sed -i '19i\  <role rolename="manager"/>' $tomcat7_userControlFile
+#sed -i '20i\  <role rolename="admin"/>' $tomcat7_userControlFile
+#sed -i '21i\  <role rolename="manager-gui"/>' $tomcat7_userControlFile
+#sed -i "22i\  $userInfo" $tomcat7_userControlFile
 #------------------------------------------------------------------------------------------------------
 
 # JK module -------------------------------------------------------------------------------------------
@@ -46,10 +46,10 @@ MACHINE_TYPE=`uname -m`
 if [ ${MACHINE_TYPE} == 'x86_64' ]
 then
   echo "64bit"
-  newJavaHome="/usr/lib/jvm/java-7-openjdk-amd64"
+  newJavaHome="/usr/lib/jvm/java-7-openjdk-amd64/"
 else
   echo "32bit"
-  newJavaHome="/usr/lib/jvm/java-7-openjdk-i386"
+  newJavaHome="/usr/lib/jvm/java-7-openjdk-i386/"
 fi
 
 #sed -i -e 's#/usr/lib/jvm/default-java#'$newJavaHome'#g' $modJK_workerPropertyFile
@@ -57,21 +57,26 @@ fi
 
 # server xml ------------------------------------------------------------------------------------------
 serverXml="/var/lib/tomcat7/conf/server.xml"
-sed -i '97i\    <Connector port="8009" protocol="AJP/1.3" redirectPort="8443" />' $serverXml
+#sed -i '97i\    <Connector port="8009" protocol="AJP/1.3" redirectPort="8443" />' $serverXml
 #------------------------------------------------------------------------------------------------------
 #======================================================================================================
 
 
 
 #    GeoServer setup    ===============================================================================
-#mkdir -p ./downloads/geoserver
-#wget http://sourceforge.net/projects/geoserver/files/GeoServer/2.6.0/geoserver-2.6.0-war.zip/download -O ./downloads/geoserver.zip
-#unzip ./downloads/geoserver.zip -d ./downloads/geoserver
-#unzip ./downloads/geoserver/geoserver.war -d /var/lib/tomcat7/webapps/geoserver
+mkdir -p ./downloads/geoserver
+wget http://sourceforge.net/projects/geoserver/files/GeoServer/2.6.0/geoserver-2.6.0-war.zip/download -O ./downloads/geoserver.zip
+unzip ./downloads/geoserver.zip -d ./downloads/geoserver
+cp ./downloads/geoserver/geoserver.war /var/lib/tomcat7/webapps/geoserver.war
 
-#service tomcat7 restart
+service tomcat7 restart
 #======================================================================================================
 
+
+#    Apache + Tomcat7 Connection 
+apacheSettingFile="/etc/apache2/sites-available/default"
+sed -i '23i\        JKMount /geoserver/* ajp13_worker' $tomcat7_userControlFile
+service apache2 restart
 
 
 
