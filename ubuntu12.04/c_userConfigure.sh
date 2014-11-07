cb6d8ad7d1eda3f578001c3ae69bacef7fe44d6b
motobyus <motobyus@naver.com>
2014-11-07 06:28:34 +0000

    add: server.xml

--- ubuntu12.04/c_userConfigure.sh
+++ ubuntu12.04/c_userConfigure.sh
@@ -25,16 +25,19 @@ done < $settingFilename
 
 
 #    tomcat7 setup    =================================================================================
+# user ------------------------------------------------------------------------------------------------
 tomcat7_userControlFile="/var/lib/tomcat7/conf/tomcat-users.xml"
 userInfo="<user username=\"$username\" password=\"$password\" roles=\"admin,manager,manager-gui\"/>"
 
-sed -i '19i\  <role rolename="manager"/>' $tomcat7_userControlFile
-sed -i '20i\  <role rolename="admin"/>' $tomcat7_userControlFile
-sed -i '21i\  <role rolename="manager-gui"/>' $tomcat7_userControlFile
-sed -i "22i\  $userInfo" $tomcat7_userControlFile
+#sed -i '19i\  <role rolename="manager"/>' $tomcat7_userControlFile
+#sed -i '20i\  <role rolename="admin"/>' $tomcat7_userControlFile
+#sed -i '21i\  <role rolename="manager-gui"/>' $tomcat7_userControlFile
+#sed -i "22i\  $userInfo" $tomcat7_userControlFile
+#------------------------------------------------------------------------------------------------------
 
+# JK module -------------------------------------------------------------------------------------------
 modJK_workerPropertyFile="/etc/libapache2-mod-jk/workers.properties"
-sed -i -e 's/tomcat6/tomcat7/g' $modJK_workerPropertyFile
+#sed -i -e 's/tomcat6/tomcat7/g' $modJK_workerPropertyFile
 
 oldJavaHome="/usr/lib/jvm/default-java"
 newJavaHome=""
@@ -49,19 +52,13 @@ else
   newJavaHome="/usr/lib/jvm/java-7-openjdk-i386"
 fi
 
-sed -i -e 's#/usr/lib/jvm/default-java#'$newJavaHome'#g' $modJK_workerPropertyFile
+#sed -i -e 's#/usr/lib/jvm/default-java#'$newJavaHome'#g' $modJK_workerPropertyFile
+#------------------------------------------------------------------------------------------------------
+
+# server xml ------------------------------------------------------------------------------------------
+serverXml="/var/lib/tomcat7/conf/server.xml"
+sed -i '97i\    <Connector port="8009" protocol="AJP/1.3" redirectPort="8443" />' $serverXml
+#------------------------------------------------------------------------------------------------------
 #======================================================================================================
-
-
-
-
-
-
-
-
-
-
-
-
 
 
