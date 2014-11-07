2b17692700672c85cd5f7c307f377994104b494c
motobyus <motobyus@naver.com>
2014-11-07 04:30:38 +0000

    add: tomcat7 setting

--- ubuntu12.04/c_userConfigure.sh
+++ ubuntu12.04/c_userConfigure.sh
@@ -32,6 +32,24 @@ sed -i '19i\  <role rolename="manager"/>' $tomcat7_userControlFile
 sed -i '20i\  <role rolename="admin"/>' $tomcat7_userControlFile
 sed -i '21i\  <role rolename="manager-gui"/>' $tomcat7_userControlFile
 sed -i "22i\  $userInfo" $tomcat7_userControlFile
+
+modJK_workerPropertyFile="/etc/libapache2-mod-jk/workers.properties"
+sed -i -e 's/tomcat6/tomcat7/g' $modJK_workerPropertyFile
+
+oldJavaHome="/usr/lib/jvm/default-java"
+newJavaHome=""
+
+MACHINE_TYPE=`uname -m`
+if [ ${MACHINE_TYPE} == 'x86_64' ]
+then
+  echo "64bit"
+  newJavaHome="/usr/lib/jvm/java-7-openjdk-amd64"
+else
+  echo "32bit"
+  newJavaHome="/usr/lib/jvm/java-7-openjdk-i386"
+fi
+
+sed -i -e 's#/usr/lib/jvm/default-java#'$newJavaHome'#g' $modJK_workerPropertyFile
 #======================================================================================================
 
 
