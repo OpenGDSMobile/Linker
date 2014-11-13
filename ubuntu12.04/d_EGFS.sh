0f1e3b37398af0d8fd20b1f543ad0e45b2e4c700
motobyus <lainer23@naver.com>
2014-11-13 13:07:44 +0000

    egov setting update

--- ubuntu12.04/d_EGFS.sh
+++ ubuntu12.04/d_EGFS.sh
@@ -23,12 +23,26 @@ done < $settingFilename
 
 
 #### e-government Framework Donwload ================================================================== 
+
+# e-gov download --------------------------------------------------------------------------------------
 mkdir -p $egovFrameworkLocation
 mkdir -p ./downloads/egovframe
 
 wget http://175.114.128.46/publist/HDD1/public/mvnrepository_3.0.zip \
      -O ./downloads/egovframe/mvnrepository_3.0.zip
 unzip ./downloads/egovframe/mvnrepository_3.0.zip -d $egovFrameworkLocation
+#------------------------------------------------------------------------------------------------------
+
+
+# e-gov maven setting ---------------------------------------------------------------------------------
+cp ../v_egovMavenSettingXml/settings.xml $egovFrameworkLocation
+
+egovMavenSettingXml="settings.xml"
+egovMavenSettingXmlFile=$egovFrameworkLocation$egovMavenSettingXml
+
+sed -i -e 's#egovMavenRepositoryDirectory#'$egovFrameworkLocation'#g' $egovMavenSettingXmlFile
+#------------------------------------------------------------------------------------------------------
+
 #======================================================================================================
 
 
